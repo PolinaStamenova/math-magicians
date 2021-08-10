3de45db93135c570fde4ca6a867091436621540c
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-10 18:36:55 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-import Calculator from "./components/Calculator";
-import "./components/Calculator.css";
+/* eslint-disable max-classes-per-file, react/prefer-stateless-function */
+
+import React from 'react';
+import Calculator from './components/Calculator';
+import './components/Calculator.css';
 
 class App extends React.Component {
   render() {
