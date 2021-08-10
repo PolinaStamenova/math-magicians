b2e8fe0835d51f3f492d4f8248529c1b6e533af5
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-10 18:56:56 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 /* eslint-disable max-classes-per-file, react/prefer-stateless-function */
-
 import React from 'react';
 import Calculator from './components/Calculator';
 import './components/Calculator.css';
