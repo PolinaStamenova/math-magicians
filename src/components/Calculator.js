02d1a57e8a0a80d8275b92f67db40d7f5d0702d1
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-11 18:27:25 +0300

    Give name to the buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Calculator.css';
+import React from "react";
+import "./Calculator.css";
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -16,61 +16,61 @@ class Calculator extends React.Component {
       <div className="calculator">
         <p className="calculator-result">{result}</p>
         <div className="calculator-buttons">
-          <button type="button" className="grey">
+          <button name="AC" type="button" className="grey">
             AC
           </button>
-          <button type="button" className="grey">
+          <button name="+/-" type="button" className="grey">
             +/-
           </button>
-          <button type="button" className="grey">
+          <button name="" type="button" className="grey">
             %
           </button>
-          <button type="button" className="orange">
+          <button name="/" type="button" className="orange">
             /
           </button>
-          <button type="button" className="grey">
+          <button name="7" type="button" className="grey">
             7
           </button>
-          <button type="button" className="grey">
+          <button name="8" type="button" className="grey">
             8
           </button>
-          <button type="button" className="grey">
+          <button name="9" type="button" className="grey">
             9
           </button>
-          <button type="button" className="orange">
+          <button name="x" type="button" className="orange">
             x
           </button>
-          <button type="button" className="grey">
+          <button name="4" type="button" className="grey">
             4
           </button>
-          <button type="button" className="grey">
+          <button name="5" type="button" className="grey">
             5
           </button>
-          <button type="button" className="grey">
+          <button name="6" type="button" className="grey">
             6
           </button>
-          <button type="button" className="orange">
+          <button name="-" type="button" className="orange">
             -
           </button>
-          <button type="button" className="grey">
+          <button name="1" type="button" className="grey">
             1
           </button>
-          <button type="button" className="grey">
+          <button name="2" type="button" className="grey">
             2
           </button>
-          <button type="button" className="grey">
+          <button name="3" type="button" className="grey">
             3
           </button>
-          <button type="button" className="orange">
+          <button name="+" type="button" className="orange">
             +
           </button>
-          <button type="button" className="grey, zero">
+          <button name="0" type="button" className="grey, zero">
             0
           </button>
-          <button type="button" className="grey">
+          <button name="." type="button" className="grey">
             .
           </button>
-          <button type="button" className="orange">
+          <button name="=" type="button" className="orange">
             =
           </button>
         </div>
