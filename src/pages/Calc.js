447bd5ea6ec7bbda1c53ca40cea0eb8a1a6298f5
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-17 20:20:45 +0300

    Remove Router tag from index.js., Change func. to arrow func.

--- src/pages/Calc.js
+++ src/pages/Calc.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Calculator from '../components/Calculator';
 import './Calc.css';
 
-function Calc() {
-  return (
-    <div className="calc-holder">
-      <h2 className="sub-title">Let`s do some math!</h2>
-      <Calculator />
-    </div>
-  );
-}
+const Calc = () => (
+  <div className="calc-holder">
+    <h2 className="sub-title">Let`s do some math!</h2>
+    <Calculator />
+  </div>
+);
 
 export default Calc;
