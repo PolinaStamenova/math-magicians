6c3b7a317ebd6b712737dfe64d0679c6e6625fdd
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-19 19:07:29 +0300

    Fix linters

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,57 +1,57 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
-describe("Operators", () => {
-  it("test if 2 + 2 = 4", () => {
+describe('Operators', () => {
+  it('test if 2 + 2 = 4', () => {
     const x = 2;
     const y = 2;
 
-    const result = operate(x, y, "+");
+    const result = operate(x, y, '+');
 
-    expect(result).toBe("4");
+    expect(result).toBe('4');
   });
 
-  it("test if 4 - 1 = 3", () => {
+  it('test if 4 - 1 = 3', () => {
     const x = 4;
     const y = 1;
 
-    const result = operate(x, y, "-");
+    const result = operate(x, y, '-');
 
-    expect(result).toBe("3");
+    expect(result).toBe('3');
   });
 
-  it("test if -2 + 2 = 0", () => {
+  it('test if -2 + 2 = 0', () => {
     const x = -2;
     const y = 2;
 
-    const result = operate(x, y, "+");
+    const result = operate(x, y, '+');
 
-    expect(result).toBe("0");
+    expect(result).toBe('0');
   });
 
-  it("test if 2 x 2 = 4", () => {
+  it('test if 2 x 2 = 4', () => {
     const x = 2;
     const y = 2;
 
-    const result = operate(x, y, "x");
+    const result = operate(x, y, 'x');
 
-    expect(result).toBe("4");
+    expect(result).toBe('4');
   });
 
-  it("test if 4 ÷ 2 = 2", () => {
+  it('test if 4 ÷ 2 = 2', () => {
     const x = 4;
     const y = 2;
 
-    const result = operate(x, y, "÷");
+    const result = operate(x, y, '÷');
 
-    expect(result).toBe("2");
+    expect(result).toBe('2');
   });
 
-  it("test if 3 % 2 = 1", () => {
+  it('test if 3 % 2 = 1', () => {
     const x = 3;
     const y = 2;
 
-    const result = operate(x, y, "%");
+    const result = operate(x, y, '%');
 
-    expect(result).toBe("1");
+    expect(result).toBe('1');
   });
 });
